09670f8a4fa8a610e456082c3ad771ac48348005
Sparsh-Dokania <sparshdokania2004@gmail.com>
2026-06-23 13:56:10 +0530

    changes done

--- src/lib/lenis.ts
+++ src/lib/lenis.ts
@@ -7,7 +7,7 @@ if (typeof window !== 'undefined') {
 }
 
 export function initLenis() {
-  const lenis = new Lenis({ lerp: 0.08, smoothWheel: true });
+  const lenis = new Lenis({ lerp: 1, smoothWheel: false });
   lenis.on('scroll', () => ScrollTrigger.update());
   gsap.ticker.add((time) => lenis.raf(time * 1000));
   gsap.ticker.lagSmoothing(0);
